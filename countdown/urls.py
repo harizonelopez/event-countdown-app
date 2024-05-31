0eeb8783ff43b1f82b1dc81a5e809b4bf16726fb
Harizone lopez <harizonelopez23@gmail.com>
2024-05-31 13:03:53 +0300

    update

--- countdown/urls.py
+++ countdown/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  
-    path('event/', views.event_detail, name='event'), 
+    path('event/', views.event_detail, name='event'),
+    path('delete/<int:event_id>/', views.delete_event, name='delete_event'),   
 ]
--- countdown/views.py
+++ countdown/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Event
 from django.utils import timezone
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 def home(request):
-    upcoming_event = Event.objects.filter(event_datetime__gte=timezone.now()).order_by('event_datetime').first()
-    upcoming_events = Event.objects.filter(event_datetime__gte=timezone.now()).order_by('event_datetime')[1:]
-    return render(request, 'home.html', {'upcoming_event': upcoming_event, 'upcoming_events': upcoming_events})
+    upcoming_events = Event.objects.filter(event_datetime__gte=timezone.now()).order_by('event_datetime')
+    events_json = json.dumps(list(upcoming_events.values('id', 'event_datetime')), cls=DjangoJSONEncoder)
+    return render(request, 'home.html', {'upcoming_events': upcoming_events, 'events_json': events_json})
+
 
 def event_detail(request):
     if request.method == 'POST':
@@ -16,3 +19,8 @@ def event_detail(request):
         return redirect('home')
     
     return render(request, 'event_detail.html')
+
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    event.delete()
+    return redirect('home')
